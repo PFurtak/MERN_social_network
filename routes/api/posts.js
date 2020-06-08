2bfc79bf7f01c0fc784b2240f9f15d31145e07c5
Patrick Furtak <patrick@patrickfurtak.com>
2020-06-08 16:15:23 -0400

    Added in likes to post, the user is added to an array on the post. Verified in postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,4 +91,28 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+//@route PUT api/posts/likes/:id
+//@desc like a post by id
+//@access private
+router.put('/likes/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // check if post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
