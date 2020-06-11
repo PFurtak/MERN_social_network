a678b7d0c8fa9ebfbbd99f4cfb3eae4772a54f8f
Patrick Furtak <patrick@patrickfurtak.com>
2020-06-10 23:14:07 -0400

    Added in alerts

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,7 +20,7 @@ const Register = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== passwordConfirm) {
-      console.log('Password mismatch');
+      setAlert('Password mismatch', 'danger');
     } else {
       console.log(formData);
     }
@@ -82,4 +85,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.protoTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
